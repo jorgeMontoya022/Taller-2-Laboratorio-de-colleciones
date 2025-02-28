d5e2557d088c446629b79d96ba24515ea3f91a97
JuanesCardenas <juane.cardenasp@uqvirtual.edu.co>
2025-02-28 08:38:31 -0500

    ejercicio2 completado

--- src/main/java/laboratorio_app/ejercicio2/Main.java
+++ src/main/java/laboratorio_app/ejercicio2/Main.java
@@ -1,4 +1,27 @@
 package laboratorio_app.ejercicio2;
 
 public class Main {
+
+    public static void main(String[] args){
+
+        PilaTipo pilaExclusiva = new PilaTipo();
+
+        // Primer Objeto para agregar.
+        pilaExclusiva.agregarPila(6);
+
+        pilaExclusiva.agregarPila(true);
+        pilaExclusiva.agregarPila(7);
+        pilaExclusiva.agregarPila("guason");
+        pilaExclusiva.agregarPila("mario");
+        pilaExclusiva.agregarPila(9.8);
+        pilaExclusiva.agregarPila(false);
+        pilaExclusiva.agregarPila(10);
+        pilaExclusiva.agregarPila(false);
+        pilaExclusiva.agregarPila("para pensar");
+
+        pilaExclusiva.imprimirPila();
+        
+
+    }
+
 }
